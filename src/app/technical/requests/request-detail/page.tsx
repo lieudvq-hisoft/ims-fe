7a3ba2ba8a2f3ccc7eb220d844760a069604d1eb
Trần Anh Tuấn <tuananhtran1410@gmail.com>
2023-10-25 23:50:51 +0700

    Router View Detail Request done

--- src/app/technical/requests/request-detail/page.tsx
+++ src/app/technical/requests/request-detail/page.tsx
@@ -1,1 +1,25 @@
- 
+// trang của technical
+
+"use client";
+import React from "react";
+import Home from "@/app/home/page";
+import ViewDetailRequestForm from "@/components/request/ViewDetailRequestForm";
+
+export default function page() {
+  return (
+    <Home
+      content={
+        <>
+          <div style={{ padding: "14px" }}>
+            <h1 style={{ paddingBottom: "10px", textAlign: "center" }}>
+              CHI TIẾT YÊU CẦU
+            </h1>
+          </div>
+          <div style={{ paddingTop: "10px" }}>
+            <ViewDetailRequestForm />
+          </div>
+        </>
+      }
+    />
+  );
+}
